ab8a0077fa8b3603beb927001d87e28e9e8a26e6
Bruno Travi Teixeira <bruno.teixeira@southsystem.com.br>
2023-07-07 02:27:34 -0300

    fix: removes useless comments

--- audio.js
+++ audio.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", (() => {
-    //var source = "https://fine.sunproxy.net/file/NmdmRkJ6cXVIUnFJaEdRSlh0Z2tSTElRZnJUdis3L2VrVzEydzVHYnBZVlBGbmdhN0kwN1hNUThWNFBhME1OSHdwVU5PMFpFK0ZhQk5QSnRIRG4xdHU0eXVwTXY1ZktDbDlrSlYvQnR5WWc9/Bee_Gees_-_Stayin_Alive_2007_Remastered_Saturday_Night_Fever_Version_(ColdMP3.com).mp3"
     var source = "./media/song.mp3"
     var audio = document.createElement("audio");
 
